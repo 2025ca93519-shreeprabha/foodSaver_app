450a93f81a41387f28424a487f0608d9195fcb75
2025ca93519-shreeprabha <2025ca93519@wilp.bits-pilani.ac.in>
2026-05-31 21:46:41 +0530

    first commit

---
+++ src/main/java/com/agile/processes/foodSaverApp/FoodSaverAppApplication.java
@@ -0,0 +1,13 @@
+package com.agile.processes.foodSaverApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSaverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodSaverAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agile/processes/foodSaverApp/FoodSaverAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agile.processes.foodSaverApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSaverAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
